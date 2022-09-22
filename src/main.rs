dd237582378f3df5dacc9614f77f21d70c599b05
ianPineda <ianpineda@ciencias.unam.mx>
2022-09-22 12:49:50 -0500

    Move read functionality into separate function

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,21 @@ impl Vertice{
     }
 }
 
-fn main() {
+fn get_file () -> String {
     let args: Vec<String> = env::args().collect();
-    
+
     let file_name = match args.get(1) {
         Some(x) => {
             x
         },
         None => {
-            println!("\n\x1b[91mNo se incluyo path para el archivo \x1b[0m");
-            println!("\n\x1b[94mLa forma correcta de ejecutar la practica es:\x1b[0m");
-            println!("cargo run [path_del_archivo]\n");
-            return
+            panic!("\x1b[91mNo se incluyo path para el archivo \x1b[0m La forma correcta de ejecutar la practica es:\x1b[94m cargo run [path_del_archivo]\x1b[0m");
         }
     };
-    let contents = fs::read_to_string(file_name)
-        .expect(&format!("\x1b[91mEl archivo {} no existe \x1b[0m", file_name));
-    
-    println!("{}", contents);
+
+    fs::read_to_string(file_name)
+        .expect(&format!("\x1b[91mEl archivo {} no existe \x1b[0m", file_name))
+}
+
+fn main() {
 }
