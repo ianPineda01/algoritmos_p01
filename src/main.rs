0d5acbfb109dddb1f7c2658a32dffc246a662e6c
ianPineda <ianpineda@ciencias.unam.mx>
2022-09-22 12:13:03 -0500

    Add functionality for reading cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 struct Vertice{
     prueba: i32
 }
@@ -11,6 +13,18 @@ impl Vertice{
 }
 
 fn main() {
-    let vertice = Vertice::new();
-    println!("{}", vertice.prueba);
+    let args: Vec<String> = env::args().collect();
+
+    
+    let file_name = match args.get(1) {
+        Some(x) => {
+            x
+        },
+        None => {
+            println!("\n\x1b[91mNo se incluyo path para el archivo \x1b[0m");
+            println!("\n\x1b[94mLa forma correcta de ejecutar la practica es:\x1b[0m");
+            println!("cargo run [path_del_archivo]\n");
+            return
+        }
+    };
 }
